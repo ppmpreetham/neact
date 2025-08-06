923c88d03941f53b8638ef9253913e6c2796348f
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2025-08-06 13:10:54 +0530

    add: comments to better understand

--- src/main.js
+++ src/main.js
@@ -1,20 +1,24 @@
 function render(element, container) {
+    // if the element is a text, create a text node else VDOM element of that type
     const dom = element.type === "TEXT_ELEMENT" ?
         document.createTextNode("") :
         document.createElement(element.type)
 
+    // set properties on the DOM element except for "children"
     const isProperty = (key) => key !== "children"
     Object.keys(element.props)
     .filter(isProperty)
     .forEach((name) => {
         dom[name] = element.props[name]
     })
-    
+
+    // recursively render children
     element.props.children.forEach((child) => {render(child, dom)})
     container.appendChild(dom)
 }
 
 function createElement(type, props, ...children) {
+    // if the type is a string, it is a DOM element, else it is a component
     return {
         type,
         props: {
@@ -27,6 +31,7 @@ function createElement(type, props, ...children) {
 }
 
 function createTextElement(text) {
+  // type "TEXT_ELEMENT" is what we use to represent text nodes in the VDOM
     return {
         type: "TEXT_ELEMENT",
         props: {
@@ -41,11 +46,13 @@ const Neact = {
 }
 
 const element = Neact.createElement(
+  // we're using babel to transform JSX into Neact.createElement calls
   <div id="div">
     <a>this is a link</a>
     <b />
   </div>
 )
 
+// container holds the root DOM element where to render the VDOM
 const container = document.getElementById("root")
-NeactDOM.render()
+NeactDOM.render(element, container)
